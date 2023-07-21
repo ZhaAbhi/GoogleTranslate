40253def915de3d1f9ec6751c1b92bd328ce1678
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-07-21 14:31:15 +0545

    Configured vector icons and stack navigation packages

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Hello world!</Text>
+      <Icon name="retweet" size={25} />
     </SafeAreaView>
   );
 };
